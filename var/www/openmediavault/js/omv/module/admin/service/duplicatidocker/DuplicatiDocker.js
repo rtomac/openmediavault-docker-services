5bb0d6b63c1e5420287c2a7c6360e1b4b595db05
Ryan Tomac <ryan@tomacfamily.com>
2017-12-21 08:00:12 -0800

    Remove duplicati GUI module

--- var/www/openmediavault/js/omv/module/admin/service/duplicatidocker/DuplicatiDocker.js
+++
@@ -1,9 +0,0 @@
-// require("js/omv/WorkspaceManager.js")
-
-OMV.WorkspaceManager.registerNode({
-    id      : "duplicatidocker",
-    path    : "/service",
-    text    : _("Duplicati"),
-    icon16  : "images/duplicati.png",
-    iconSvg : "images/duplicati.svg"
-});
